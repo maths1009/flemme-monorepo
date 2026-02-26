6f69a6cf1eb36514f6f6b3d22fe1b94cd22972d7
Tisma <lb.brangeon@gmail.com>
2026-02-26 11:12:06 +0100

    💚 fiix

--- apps/web/app/auth/login/success/page.tsx
+++ apps/web/app/auth/login/success/page.tsx
@@ -1,22 +1,21 @@
 'use client';
 
-import { CircularButton } from '@/components/auth/CircularButton';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { CircularButton } from '@/components/auth/CircularButton';
 
 const LoginSuccessPage = () => {
   const router = useRouter();
 
   return (
     <div className="relative mx-auto flex h-screen max-w-[390px] flex-col items-center bg-primary/5 px-6 py-10 space-y-10 mt-[48px]">
-      
       <div className="mt-6">
         <Image
-          src="/images/auth/successSVG.svg"
           alt="illustration"
-          width={260}
-          height={200}
           className="h-auto w-[200px]"
+          height={200}
+          src="/images/auth/successSVG.svg"
+          width={260}
         />
       </div>
 
